1998183cc7ae4b45606f919e7d2f0ea49223e4c4
skahwaji <skahwaji@sharptask.com>
2021-09-23 17:30:55 +0300

    Initial Commit

---
+++ HALHelpers/DataPager/DataPagerExtension.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HALHelpers.DataPager
+{
+    public class PagedModel<TModel>
+    {
+        const int MaxPageSize = 500;
+        private int _pageSize;
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+
+        public int CurrentPage { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public IList<TModel> Items { get; set; }
+
+        public PagedModel()
+        {
+            Items = new List<TModel>();
+        }
+    }
+
+    public static class DataPagerExtension
+    {
+        public static async Task<PagedModel<TModel>> PaginateAsync<TModel>(
+            this IQueryable<TModel> query,
+            int page,
+            int limit,
+            CancellationToken cancellationToken)
+            where TModel : class
+        {
+
+            var paged = new PagedModel<TModel>();
+
+            page = (page < 0) ? 1 : page;
+
+            paged.CurrentPage = page;
+            paged.PageSize = limit;
+
+            paged.TotalItems = await query.CountAsync(cancellationToken);
+
+            var startRow = (page - 1) * limit;
+            paged.Items = await query
+                       .Skip(startRow)
+                       .Take(limit)
+                       .ToListAsync(cancellationToken);
+
+            paged.TotalPages = (int)Math.Ceiling(paged.TotalItems / (double)limit);
+
+            return paged;
+        }
+    }
+}
---
+++ HALHelpers/ResourceLinks/ResourceLinks.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+
+namespace HALHelpers.ResourceLinks
+{
+    public record ResourceHref(string Href);
+    public record ResourceTemplated(string Href, bool Templated);
+    public record ResourceCuries(string Name, string Href, bool Templated);
+
+    public interface IResourceLinksExtension 
+    {
+        IDictionary<string, object> _links { get; set; }
+    }
+
+    public interface IResourceEembeddedExtension 
+    {
+        List<object> _embedded { get; set; }
+    }
+
+    public interface IResourceCuriesExtension
+    {
+        List<object> curies { get; set; }
+    }
+
+    public static class ResourceLinks
+    {
+        public static void AddLink(this IResourceLinksExtension record, string type, string routeUrl, bool templated = false)
+        {
+            record._links ??= new Dictionary<string, object>();
+            if(!templated)
+                record._links[type] = new ResourceHref(routeUrl);
+            else
+                record._links[type] = new ResourceTemplated(routeUrl, templated);
+        }
+
+        public static void AddObjectLink(this IResourceLinksExtension record, string type, object obj)
+        {
+            record._links ??= new Dictionary<string, object>();
+            record._links[type] = obj;
+        }
+
+        public static void AddEmbedded(this IResourceEembeddedExtension record, object obj)
+        {
+            record._embedded ??= new List<object>();
+            record._embedded.Add(obj);
+        }
+    }
+}
